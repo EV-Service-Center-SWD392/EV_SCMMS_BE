27242280ca93d31068a8181deb468aaa394beeca
huynh tu <huynhthaitu124@gmail.com>
2025-10-24 10:09:35 +0700

    add generic booking repo

---
+++ src/EV_SCMMS.Core/Application/Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EV_SCMMS.Core.Domain.Models;
+
+namespace EV_SCMMS.Core.Application.Interfaces.Repositories
+{
+    public interface IBookingRepository : IGenericRepository<Bookinghuykt>
+    {
+        
+    }
+}
